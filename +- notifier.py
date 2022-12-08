84c9f6859d1f73ffb59240b27dfea938119cea3f
4sight <80678178+maxzysparks@users.noreply.github.com>
2022-12-08 12:07:01 +0100

    Update and rename +- notifier to +- notifier.py

--- +- notifier.py
+++ +- notifier.py
@@ -1,11 +1,14 @@
 def check_price(token: str, threshold: float):
+  
   # Query current price of token
   current_price = get_current_price(token)
   
   # Check if current price is above or below threshold
   if current_price >= threshold:
+    
     # Send notification that price has increased
     send_notification(f'{token} price has increased!')
   elif current_price <= threshold:
+    
     # Send notification that price has decreased
     send_notification(f'{token} price has decreased!')
